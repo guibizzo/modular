11e4edf8c4daccc485f2d2a8f0ab9d4ce69ffa8c
guibizzo <62184670+guibizzo@users.noreply.github.com>
2020-09-22 19:38:01 -0300

    Create Peão.py

---
+++ Peão.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Trabalho Ludo - Programação Modular 2020.2
+Grupo 1:
+    Daniel Guimarães
+    Gabriel Galvão
+    Guilherme Bizzo
+"""
+
+
+class Peao:
+    
+    def __init__(self, pos, protegido, inicio):
+        self.pos = pos
+        self.protegido = protegido
+        self.inicio = inicio
+        
+        
